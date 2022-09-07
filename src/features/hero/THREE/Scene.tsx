07947a49dd284d99489a125971770c7fbec4aa46
Oded Winberger <odedindi@gmail.com>
2022-09-07 11:59:05 +0200

    add mobile flexibily

--- src/features/hero/THREE/Scene.tsx
+++ src/features/hero/THREE/Scene.tsx
@@ -4,14 +4,15 @@ import * as React from "react"
 import { Stats } from "@react-three/drei"
 import { Canvas, Dpr } from "@react-three/fiber"
 import { Effects } from "../THREE/Effects"
-import { useDevicePixelRatio } from "@/hooks/useDevicePixelRatio"
+
+import { useIsMobile } from "@/hooks/useIsMobile"
 
 const Sphere = dynamic(() => import("../THREE/Sun"), { ssr: false })
 
 export const Scene: React.FC = () => {
-	const dpr = useDevicePixelRatio()
+	const isMobile = useIsMobile()
 	return (
-		<Canvas dpr={dpr} gl={{ antialias: false }}>
+		<Canvas dpr={isMobile ? 1 : 2} gl={{ antialias: false }}>
 			<Sphere />
 			<Effects />
 			{process.env.NODE_ENV === "development" && <Stats />}
