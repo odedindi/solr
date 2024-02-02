3a72a653c792b4df729f407614489fd6c84e2924
odi <odi@panter.ch>
2024-02-02 21:02:27 +0100

    set body style to hide overflow

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class _Document extends Document {
 		return (
 			<Html>
 				<Head />
-				<body>
+				<body style={{ overflow: "hidden" }}>
 					<Main />
 					<NextScript />
 				</body>
